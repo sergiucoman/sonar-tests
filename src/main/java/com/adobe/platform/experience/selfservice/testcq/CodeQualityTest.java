694c2ed28cd86115da2c327095ab09e3cf1f2c65
scoman <scoman@adobe.com>
2023-07-05 15:22:37 +0300

    fixed errors

--- src/main/java/com/adobe/platform/experience/selfservice/testcq/CodeQualityTest.java
+++ src/main/java/com/adobe/platform/experience/selfservice/testcq/CodeQualityTest.java
@@ -1,3 +1,40 @@
-package com.adobe.platform.experience.selfservice.testcq;public class CodeQualityTest {
+package com.adobe.platform.experience.selfservice.testcq;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class CodeQualityTest {
   private static final String NAME="123";
+
+  public static int MAX_COUNT = 0;
+  
+  public int task1(int a, int b) {
+    return a + b;
+  }
+
+  public int task2(int a, int b) {
+    int sum = a  + b;
+    return sum;
+  }
+
+  public void printData(List<String> data) {
+    if (data.size() > MAX_COUNT) {
+      for (String value : data) {
+        System.out.println(value);
+      }
+    }
+  }
+
+  public void processFile(String filePath) {
+    try {
+      File file = new File(filePath);
+      // Other file processing logic...
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
